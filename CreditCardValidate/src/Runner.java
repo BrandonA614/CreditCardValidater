2071974c7d9ee70cccf1000f317d371136c300c7
BrandonA614 <brandon.ashkin@darbygroup.com>
2016-10-26 14:55:49 -0600

    Finish tonight - - Lots to do

--- CreditCardValidate/src/Runner.java
+++ CreditCardValidate/src/Runner.java
@@ -1,46 +1,37 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.*;
 
 public class Runner
 	{
-	
+	//type.toString(input) to change from int / long / double etc to a string - - Can then go through it and .substring to grab specific numbers
+		static int sum;
+
 		
 		public static void main(String[] args) throws FileNotFoundException
 			{
-				Scanner file = new Scanner( new File( "creditCardNumbers.txt" ) );
+				`	
 				
 				for(int i = 0; i< 100; i++){
 				long num = file.nextLong();
-				System.out.println(num);
+
 					while(num > 0) {
-					long digit = num % 10;
-					digit = num / 10;
-					digit = num %10;
-					System.out.println(digit);
-					digit = num % 10;
-					
-					System.out.println(digit);
-					break;
-	
+//					long digit = num % 10;
+//					digit = num / 10;
+//					digit = num %10;
+//					long digit1 = num % 10;
+//					System.out.println(numberAsString);
+//					long digit2 = digit1 % 10;
+//					
+//					System.out.println(digit2);
+//					break;
+	LunchCheckDigit();
 
 				
 			}
-				}
+				
 			
 	}
-		public static void stepOne() throws FileNotFoundException
-			{
-			Scanner file = new Scanner( new File( "creditCardNumbers.txt" ) );
-			long numSelection = file.nextLong();
-			while(numSelection > 0) {
-				long digit = numSelection % 10;
-				digit = digit /10;
-				System.out.println(digit);
-				break;
-			}
-			
-			
-			
-		}
-}
+
+
 		
